249b809c4c78fe4b723fd1b86c34119bca6ab1c5
emanuelpinheiro <86086371+emanuelpinheiro@users.noreply.github.com>
2025-04-11 22:43:24 -0300

    Adicionar arquivos de projeto.

---
+++ FutebolLigaTabajara/Controllers/TimesController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FutebolLigaTabajara.Controllers
+{
+    public class TimesController
+    {
+    }
+}
---
+++ FutebolLigaTabajara/LigaDBContext.cs
@@ -0,0 +1,21 @@
+﻿using FutebolLigaTabajara.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FutebolLigaTabajara
+{
+    public class LigaDBContext : DbContext
+    {
+        public DbSet<Time> Times { get; set; }
+        public DbSet<Jogador> Jogadores { get; set; }
+        public DbSet<ComissaoTecnica> ComissaoTecnica { get; set; }
+        public DbSet<Partida> Partidas { get; set; }
+        public DbSet<Estatistica> Estatisticas { get; set; }
+
+        public LigaDBContext() : base("DefaultConnection") { }
+    }
+
+}
